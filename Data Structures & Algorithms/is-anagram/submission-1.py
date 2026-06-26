eacc9a4e67b085bc499053d3e6d99734c82b9fa9
xzhongMM <129787511+xzhongMM@users.noreply.github.com>
2026-06-26 11:14:47 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_occurences = {}
+        for letter in s:
+            if letter not in s_occurences:
+                s_occurences[letter] = 0
+            s_occurences[letter] += 1
+        
+        for letter in t:
+            if letter not in s_occurences:
+                return False
+            s_occurences[letter] -= 1
+            if s_occurences[letter] < 0:
+                return False
+            
+        for l in s_occurences:
+            if s_occurences[l] != 0:
+                return False
+        return True
