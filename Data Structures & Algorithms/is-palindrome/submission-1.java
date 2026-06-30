8862307da8af6274f65fc965b5c4a350dc154f9e
xzhongMM <129787511+xzhongMM@users.noreply.github.com>
2026-06-30 20:34:58 +0800

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int size = s.length();
+
+        for(int i = 0; i<size/2; i++)
+            if(s.charAt(i) != s.charAt(size-i-1)){
+                return false;
+            }
+
+        return true;
+    }
+}
