f1c5b9eb52ae37a88d3735f2a9a96580dc8d45ad
xzhongMM <129787511+xzhongMM@users.noreply.github.com>
2026-07-27 23:48:25 +0800

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        left = 0
+        right = 1
+
+        s1freqs = [0]*26
+        for s in s1:
+            s1freqs[ord(s) - 97] += 1
+
+        s2freqs = [0]*26
+
+        while right <= len(s2):
+            #update freq of new character
+            s2freqs[ord(s2[right-1]) - 97] += 1
+            #if substrings not of len(s1), increment right until we get substring of correct length
+            while right - left < len(s1):
+                right += 1
+                s2freqs[ord(s2[right-1]) - 97] += 1
+            #if it's correct length, check if two freq lists match
+            if s1freqs == s2freqs:
+                return True
+            else:
+                #if not increment left and right and update frequenciy of left character
+                s2freqs[ord(s2[left]) - 97] -= 1
+                left += 1
+                right += 1
+        return False
+            
